93a965e0118595609df61fe7bbe9a79b24a7170e
MovieBots <154045205+MovieBots@users.noreply.github.com>
2025-06-25 18:30:48 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -242,8 +242,7 @@ Nᴀᴍᴇ : {}
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
     CAPTION = """<b><a href="https://t.me/Arpitbotmovies">📂Fɪʟᴇɴᴀᴍᴇ : {file_name}</a>  
-    
-Jᴏɪɴ Us ➠ @Film_CityOfficial"""
+    Jᴏɪɴ Us ➠ @Film_CityOfficial"""
 
     IMDB_TEMPLATE_TXT = """
 <b>🏷 Title</b>: <a href={url}>{title}</a>
