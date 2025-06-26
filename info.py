699423819de171d6c748777e663162f2f97dc750
MovieBots <154045205+MovieBots@users.noreply.github.com>
2025-06-26 21:23:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -121,7 +121,7 @@ if 'DYNO' in environ:
     APP_NAME = environ.get('APP_NAME')
 else:
     ON_HEROKU = False
-BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
+BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'https://brave-maud-talkmovies-de8f4596.koyeb.app/'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
 URL = "https://brave-maud-talkmovies-de8f4596.koyeb.app/".format(FQDN) if ON_HEROKU or NO_PORT else "https://brave-maud-talkmovies-de8f4596.koyeb.app/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
