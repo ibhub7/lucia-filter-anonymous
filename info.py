c8b4feccf7fb59af146b4cf843c7a0c63733c894
mimamofficial1 <muzaffarimamneema@gmail.com>
2025-12-09 15:36:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -129,7 +129,7 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://suspicious-ora-chillf-792a0655.koyeb.app/".format(FQDN) if ON_HEROKU or NO_PORT else "https://suspicious-ora-chillf-792a0655.koyeb.app/".format(FQDN, PORT)
+URL = "https://filterbot-mrniofficialx.koyeb.app/".format(FQDN) if ON_HEROKU or NO_PORT else "https://filterbot-mrniofficialx.koyeb.app/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'Mrn_Officialx'))
@@ -143,9 +143,9 @@ else:
     ON_HEROKU = False
 HAS_SSL = bool(getenv('HAS_SSL', False))
 if HAS_SSL:
-    URL = "https://suspicious-ora-chillf-792a0655.koyeb.app/".format(FQDN)
+    URL = "https://filterbot-mrniofficialx.koyeb.app/".format(FQDN)
 else:
-    URL = "https://suspicious-ora-chillf-792a0655.koyeb.app/".format(FQDN)
+    URL = "https://filterbot-mrniofficialx.koyeb.app/".format(FQDN)
 
 
 REACTIONS = ["🤝", "😇", "🤗", "😍", "👍", "🎅", "😐", "🥰", "🤩", "😱", "🤣", "😘", "👏", "😛", "😈", "🎉", "⚡️", "🫡", "🤓", "😎", "🏆", "🔥", "🤭", "🌚", "🆒", "👻", "😁"]
