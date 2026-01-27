8ab4ab8e93deeb0ae655e578dbfe61a0c0f83efd
⌯ Ꭺɴᴏɴʏᴍᴏᴜꜱ | ×͜× | <155171427+NBBotz@users.noreply.github.com>
2026-01-28 00:36:23 +0530

    typo!

--- plugins/commands.py
+++ plugins/commands.py
@@ -283,7 +283,7 @@ async def start(client, message):
                 try:
                     f_caption=SILENTX_CAPTION.format(file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='' if f_caption is None else f_caption)
                 except Exception as e:
-                    logger.exception(e)
+                    LOGGER.exception(e)
                     f_caption = f_caption
             if f_caption is None:
                 f_caption = clean_filename(files1.file_name) 
